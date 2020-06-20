0c5960f97857686c2856edd4fcfdd5848b3bb867
Ani-PC-Ubuntu <anirudhmv21@gmail.com>
2020-06-20 23:03:43 +0530

    ledger list reordering
    ledger list now shows latest entry on the top and older entries at the bottom

--- app/src/main/java/com/example/myapp/ui/main/MyAdapter.kt
+++ app/src/main/java/com/example/myapp/ui/main/MyAdapter.kt
@@ -12,17 +12,15 @@ import com.example.myapp.R
 class MyAdapter (var mCtx:Context,var resources:Int,var items:List<Model>):ArrayAdapter<Model>(mCtx, resources,items){
 
     private val mMessagesList: List<Model>? = null
+    val itemSize = items.size
 
-    init{
-//        items = items.reversed()
-    }
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val layoutInflater:LayoutInflater= LayoutInflater.from(mCtx)
         val view:View = layoutInflater.inflate(resources, null)
         val imageView:ImageView = view.findViewById(R.id.image)
         val titleTextView:TextView = view.findViewById(R.id.textView1)
         val descriptionTextView:TextView = view.findViewById(R.id.textView2)
-        var mItem:Model = items[position]
+        var mItem:Model = items[itemSize - position - 1]        // to show the latest ledger item on the top of the list
         imageView.setImageDrawable(mCtx.resources.getDrawable(mItem.img))
         titleTextView.text = mItem.locationName
         descriptionTextView.text = mItem.landmarkName
